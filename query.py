fe4d40f06ae2ff577b94ba2b68c444b08ec8743e
Megan LaRoy <145464737+meglikeeggs@users.noreply.github.com>
2023-11-02 00:30:46 +0000

    first real commit

--- query.py
+++ query.py
@@ -3,11 +3,13 @@ from movies import Movies
 movies = Movies('./movies.txt')
 
 
-def list_all ():
+def list_all():
+    
 
 
 
 def list_name():
+    
 
 
 
@@ -15,23 +17,23 @@ def list_cast():
 
 
 
-input = ' '
+choice = ' '
 
-    while input.lower() != 'q':
-        print("q: quit")
-        print("sn: search by name")
-        print("sc: search by cast")
-        print("list: print all the movie names")
-        input.lower() = scan("")
+while choice.lower() != 'q':
+    print("q: quit")
+    print("sn: search by name")
+    print("sc: search by cast")
+    print("list: print all the movie names")
+    choice = input("").lower()
 
-        if(input == "list"):
-            list_all()
+    if(choice == "list"):
+        list_all()
 
-        if (input == "sn"):
-            list_name()
+    if (choice == "sn"):
+        list_name()
 
-        if (input == "sc"):
-            list_cast()
+    if (choice == "sc"):
+        list_cast()
 
 
 
