1c5f9c53281dca397246d0cfb72f3937e4e63125
Megan LaRoy <145464737+meglikeeggs@users.noreply.github.com>
2023-11-02 00:12:41 +0000

    query

--- query.py
+++ query.py
@@ -1,3 +1,50 @@
 from movies import Movies
 
 movies = Movies('./movies.txt')
+
+
+def list_all ():
+
+
+
+def list_name():
+
+
+
+def list_cast():
+
+
+
+input = ' '
+
+    while input.lower() != 'q':
+        print("q: quit")
+        print("sn: search by name")
+        print("sc: search by cast")
+        print("list: print all the movie names")
+        input.lower() = scan("")
+
+        if(input == "list"):
+            list_all()
+
+        if (input == "sn"):
+            list_name()
+
+        if (input == "sc"):
+            list_cast()
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
